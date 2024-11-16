2959c989a71baba1a59857f5a2043c719b116d13
lsok <lsokweb@gmail.com>
2024-11-16 13:12:42 +0800

    测试修改日期

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>456Congratulations!</h1>
+        <h1>恭喜!</h1>
 
         <?php
         var_dump($data);
