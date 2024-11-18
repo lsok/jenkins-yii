fa2c23eb2c09acb30030de248e731626f275ca8a
lsok <lsokweb@gmail.com>
2024-11-18 10:05:12 +0800

    test dev

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>只传输指定目录!</h1>
+        <h1>这是dev分支!</h1>
 
         <?php
         var_dump($data);
